276f2dbff15ffbaca2d6eb6f8e1dd5e93c1bfcdc
qmcco <qmccollister03@gmail.com>
2025-02-10 14:13:04 -0600

    Added Book class attributes, default, and parameterized constructors
    Added title, author, ISBN, and price attributes
    Added default Book constructor, setting all string attributes to
    "Unknown" and price to 0.0
    Added parameterized Book constructor to facilitate the creation of
    instances of Book class

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,16 @@
 
+import java.lang.String;
+
 public class Book {
+	private String title;
+	private String author;
+	private String ISBN;
+	private double price;
 	
+	public Book() {
+		title = "Unknown";
+		author = "Unknown";
+		ISBN = "Unknown";
+		price = 0.0;
+	}
 }
