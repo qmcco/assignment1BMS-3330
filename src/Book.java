75e679eaa23a79fdda3e8ea69fe81d46f048f9cc
qmcco <qmccollister03@gmail.com>
2025-02-11 14:48:27 -0600

    Added methods to get and set Book class elements
    Added getTitle(), getAuthor(), getISBN(), getPrice() which return the
    current value of the respective Book class elements of the referenced
    instance of Book class
    Added setTitle(), setAuthor(), setISBN, setPrice() to set the value of
    the Book elements of the referenced instance of Book class

--- src/Book.java
+++ src/Book.java
@@ -13,4 +13,43 @@ public class Book {
 		ISBN = "Unknown";
 		price = 0.0;
 	}
+	
+	public Book(String title, String author, String ISBN, double price) {
+		this.title = title;
+		this.author = author;
+		this.ISBN = ISBN;
+		this.price = price;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getAuthor() {
+		return this.author;
+	}
+	
+	public String getISBN() {
+		return this.ISBN;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public void setISBN(String ISBN) {
+		this.ISBN = ISBN;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
 }
