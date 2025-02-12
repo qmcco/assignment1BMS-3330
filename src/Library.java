0c104a3762ba23491c06629d1bcfccde500cefcd
qmcco <qmccollister03@gmail.com>
2025-02-12 14:15:51 -0600

    Added attributes, addBook(Book book) method to Library class
    Added Book[] books and int count attributes to Library class
    Added addBook(Book book) method to Library class, with functionality to
    add a book to the books array of the given instance of Library class
    given space is available

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,20 @@
 
 public class Library {
-
+	private Book[] books = new Book[5];
+	private int count;
+	
+	public boolean addBook(Book book) {
+		int empCheck = 0;
+		for(int i = 0; i < 5; i++) {
+			if(this.books[i] == null) {
+				empCheck = i;
+			}
+		}
+		if(empCheck != 0) {
+			this.books[empCheck] = book;
+			this.count++;
+			return true;
+		}
+		return false;
+	}
 }
